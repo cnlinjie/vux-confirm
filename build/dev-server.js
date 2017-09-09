5269a22eb642ebd16b104f8f8908c3892f823017
linjie <cnlinjie@foxmail.com>
2017-09-09 22:31:10 +0800

    dev

--- build/dev-server.js
+++ build/dev-server.js
@@ -53,7 +53,6 @@ Object.keys(proxyTable).forEach(function (context) {
 
 // handle fallback for HTML5 history API
 // app.use(require('connect-history-api-fallback')())
-app.use(history(webpackConfig.devServer.historyApiFallback))
 
 // serve webpack bundle output
 app.use(devMiddleware)
